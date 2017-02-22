d3d0084e3b56d229bc3a7a645697b67b8f28eb8d
kammy92 <karman.singhh@gmail.com>
2017-02-22 16:54:51 +0530

    Working Code

--- app/src/main/java/com/actiknow/machinetest/MainActivity.java
+++ app/src/main/java/com/actiknow/machinetest/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import static java.lang.Integer.parseInt;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText etValueOfN;
     Button btGetSeries;
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
         initView ();
+        initListener ();
     }
 
     private void initView () {
@@ -31,33 +34,93 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         tvOddSum = (TextView) findViewById (R.id.tvOddSum);
     }
 
+    private void initListener () {
+        btGetSeries.setOnClickListener (this);
+        btFromAPI.setOnClickListener (this);
+    }
 
     @Override
     public void onClick (View view) {
+        int valueOfN;
+        switch (view.getId ()) {
+            case R.id.btGetSeries:
+                valueOfN = parseInt (etValueOfN.getText ().toString ());
+                tvSeries.setText (printSeries (valueOfN));
+                tvOddSum.setText ("" + getOddSum (valueOfN));
+                tvEvenSum.setText ("" + getEvenSum (valueOfN));
+                break;
+            case R.id.btFromAPI:
+                valueOfN = getValueOfNFromAPI ();
+                tvSeries.setText (printSeries (valueOfN));
+                tvOddSum.setText ("" + getOddSum (valueOfN));
+                tvEvenSum.setText ("" + getEvenSum (valueOfN));
+                break;
+        }
     }
 
     public int getValueOfNFromAPI () {
         String URL = "https://blood-connect-cammy92.c9users.io/random.php"; // generates a random number between 10-16
-        // Get a number from API
+
         return 12;
     }
 
     public int getOddSum (int n) {
         int oddSum = 0;
-        // Find sum of odd numbers in the series
+        int[] fib = new int[n];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        for (int i = 0; i < n; i++) {
+            if (fib[i] % 2 != 0) {
+                oddSum = oddSum + fib[i];
+            }
+        }
         return oddSum;
     }
 
     public int getEvenSum (int n) {
         int evenSum = 0;
-        // Find sum of even numbers in the series
+        int[] fib = new int[n];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        for (int i = 0; i < n; i++) {
+            if (fib[i] % 2 == 0) {
+                evenSum = evenSum + fib[i];
+            }
+        }
         return evenSum;
     }
 
     public String printSeries (int n) {
-        String series = "0";
-        // Return the series as a string
-        return series;
-    }
+        StringBuilder series = new StringBuilder ();
+        int[] fib = new int[n];
+        fib[0] = 0;
+        fib[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        for (int i = 0; i < n; i++) {
+            series.append (fib[i] + ",");
+        }
 
+/*
+        int n1 = 0;
+        int n2 = 1;
+        int n3;
+        series.append (n1 + ",");
+        series.append (n2 + ",");
+        for (int i = 2; i < n; ++ i) {
+            n3 = n1 + n2;
+            series.append (n3 + ",");
+            n1 = n2;
+            n2 = n3;
+        }
+*/
+        return series.toString ();
+    }
 }
